0f9a7ee875194887e6d5d74ba3d138cbbf158a00
Eli <elisego46@gmail.com>
2026-04-13 17:21:11 -0500

    get started with some simple config

---
+++ e1QuartzIntroduction/src/main/java/com/segovia/tutorials/java_quartz_exmple/e1QuartzIntroduction/E1QuartzIntroductionApplication.java
@@ -0,0 +1,14 @@
+package com.segovia.tutorials.java_quartz_exmple.e1QuartzIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1QuartzIntroductionApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(E1QuartzIntroductionApplication.class, args);
+	}
+
+}
---
+++ e1QuartzIntroduction/src/test/java/com/segovia/tutorials/java_quartz_exmple/e1QuartzIntroduction/E1QuartzIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovia.tutorials.java_quartz_exmple.e1QuartzIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1QuartzIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
